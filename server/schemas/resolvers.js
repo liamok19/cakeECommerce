57f6b5d9d706811646551a78cc1e0585a28c5593
Liam Ok <liamokane19@gmail.com>
2022-05-03 10:53:24 +1000

    added in mutations to user and login. Products and cart haven't been added yet

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -10,7 +10,7 @@ const resolvers = {
     users: async (parent, { username }) => {
       return User.findOne({ username }).populate("products");
     },
-    products: async (parents, { username }) => {
+    products: async (parent, { username }) => {
       return (params = username ? { username } : {});
       return Product.find(params).sort({ createdAt: -1 });
     },
@@ -18,4 +18,30 @@ const resolvers = {
       return Product.findOne({ _id: productId });
     },
   },
+
+  Mutation: {
+    addUser: async (paren, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError("Incorrect credentials");
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect credentials ");
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
+  },
 };
+module.exports = resolvers;
