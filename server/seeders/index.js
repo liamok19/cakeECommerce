0452e691ce61a8f4c529280bc43ea1c5002600fe
Liam Ok <liamokane19@gmail.com>
2022-05-03 11:20:11 +1000

    databse when open now is pulling in the seeded data from Users and Products

--- server/seeders/index.js
+++ server/seeders/index.js
@@ -1,2 +1,32 @@
-const db = require ('../config/connection');
-const { User, Product, Cart } = require ('../models');
+const db = require("../config/connection");
+const { User, Product, Cart } = require("../models");
+const userSeeds = require("./userSeeds.json");
+const productSeeds = require("./productSeeds.json");
+// const cartSeeds = require("./cartSeeds.json");
+
+db.once("open", async () => {
+  try {
+    await Product.deleteMany({});
+    await User.deleteMany({});
+
+    await User.create(userSeeds);
+
+    for (let i = 0; i < productSeeds.length; i++) {
+      const { _id, productName } = await Product.create(productSeeds[i]);
+      const user = await User.findOneAndUpdate(
+        { username: productName },
+        {
+          $addToSet: {
+            products: _id,
+          },
+        }
+      );
+    }
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+
+  console.log("all done!");
+  process.exit(0);
+});
