bce14feb95a2673b706ffed325dd1f1521d01d91
Liam Ok <liamokane19@gmail.com>
2022-05-06 12:57:21 +1000

    home component bringing in 3 nested components. CategoryMenu, Productlist and cart. Each component still INP

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,36 +1,53 @@
+// import React from "react";
+
+// const styles = {
+//   card1:{
+//     fontSize: 30,
+//   },
+//   card2:{
+//     fontSize: 20,
+//   },
+//   imgStyle: {
+//     width: 450,
+//     height: 300
+//   },
+//   containerStyle: {
+//     flexDirection: 'column',
+//     display: 'flex',
+//     alignItems: 'center',
+//     // color: '#3A7759',
+//     backgroundColor: '#8CAB9B',
+//     paddingRight: 75,
+//     paddingLeft: 75,
+//     paddingBottom: 270
+//     }, 
+// }
+
+// export default function Home() {
+//     return (
+//       <div style={styles.containerStyle}>
+//         <h1>Cake</h1>
+//         <p style={styles.card1}>
+//           Cakes Galore to choose from
+//         </p> 
+//         <p style={styles.card2}>Yum Yum for your tum tum</p>
+//             </div>
+//     );
+//   }
+
 import React from "react";
+import ProductList from "../components/ProductList";
+import CategoryMenu from "../components/CategoryMenu";
+import Cart from "../components/Cart";
 
-const styles = {
-  card1:{
-    fontSize: 30,
-  },
-  card2:{
-    fontSize: 20,
-  },
-  imgStyle: {
-    width: 450,
-    height: 300
-  },
-  containerStyle: {
-    flexDirection: 'column',
-    display: 'flex',
-    alignItems: 'center',
-    // color: '#3A7759',
-    backgroundColor: '#8CAB9B',
-    paddingRight: 75,
-    paddingLeft: 75,
-    paddingBottom: 270
-    }, 
-}
+const Home = () => {
+  return (
+    <div className="container">
+      <CategoryMenu />
+      <ProductList />
+      <Cart />
+    </div>
+  );
+};
 
-export default function Home() {
-    return (
-      <div style={styles.containerStyle}>
-        <h1>Cake</h1>
-        <p style={styles.card1}>
-          Cakes Galore to choose from
-        </p> 
-        <p style={styles.card2}>Yum Yum for your tum tum</p>
-            </div>
-    );
-  }
+export default Home;
