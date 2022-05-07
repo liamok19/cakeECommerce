fe0527261864b0b6db9b96cacc6a775242fe18bd
Liam Ok <liamokane19@gmail.com>
2022-05-07 13:55:41 +1000

    user schema was pulling in an error with because of the cart model. This has been updated

--- server/models/User.js
+++ server/models/User.js
@@ -1,5 +1,6 @@
 const { Schema, model } = require("mongoose");
 const bcrypt = require("bcrypt");
+const Cart = require('./Cart');
 
 const userSchema = new Schema({
   username: {
@@ -25,7 +26,7 @@ const userSchema = new Schema({
   //     ref: "Cart",
   //   },
   // ],
-  carts: [Cart.Schema]
+  carts: [Cart.Schema] 
 });
 
 userSchema.pre("save", async function (next) {
