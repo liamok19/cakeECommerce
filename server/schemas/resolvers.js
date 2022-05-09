41f4b1086c9a92cfd77c788456f80943349a89c5
Liam Ok <liamokane19@gmail.com>
2022-05-09 20:49:25 +1000

    added sessions checkout query to the resolver

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,11 +1,12 @@
 const { AuthenticationError } = require("apollo-server-express");
+const { stripIgnoredCharacters } = require("graphql");
 const { User, Cart, Product, Category } = require("../models");
 const { populate } = require("../models/Cart");
 const { signToken } = require("../utils/auth");
 
 const resolvers = {
   Query: {
-    categories: async() =>{
+    categories: async () => {
       return await Category.find();
     },
     products: async (parent, { category, name }) => {
@@ -17,49 +18,82 @@ const resolvers = {
 
       if (name) {
         params.name = {
-          $regex: name
+          $regex: name,
         };
       }
 
-      return await Product.find(params).populate('category');
-    
+      return await Product.find(params).populate("category");
+
       // return (params = username ? { username } : {});
       // return Product.find(params).sort({ createdAt: -1 });
     },
     // product: async (parent, { productId }) => {
     //   return Product.findOne({ _id: productId });
     // },
-  product: async (parent, { _id }) => {
-      return await Product.findById(_id).populate('category');
+    product: async (parent, { _id }) => {
+      return await Product.findById(_id).populate("category");
     },
 
     users: async () => {
-      
       return User.find();
     },
     user: async (parent, arsg, context) => {
-      if(context.user){
+      if (context.user) {
         const user = await User.findById(context.user._id).populate({
-          path: 'carts.products',
-          populate: 'category'
+          path: "carts.products",
+          populate: "category",
         });
 
-        user.carts.sort((a,b) => b.purchaseDate - a.purchaseDate);
+        user.carts.sort((a, b) => b.purchaseDate - a.purchaseDate);
         return user;
       }
       // return User.findOne({ username }).populate("products");
-      throw new AuthenticationError('You must be logged in')
+      throw new AuthenticationError("You must be logged in");
     },
-    cart: async (parent, {_id}, context) => {
+    cart: async (parent, { _id }, context) => {
       if (context.user) {
         const user = await User.findById(context.user._id).populate({
-          path: 'carts.products',
-          populate: 'category'
-        })
+          path: "carts.products",
+          populate: "category",
+        });
         return user.carts.id(_id);
       }
-      throw new AuthenticationError('You must be logged in')
-    }
+      throw new AuthenticationError("You must be logged in");
+    },
+    checkout: async (parent, args, context) => {
+      const url = new URL(context.headers.referer).origin;
+      const cart = new Cart({ products: args.products });
+      const line_items = [];
+
+      const { products } = await cart.populate("products");
+
+      for (let i = 0; i < products.length; i++) {
+        const product = await stripe.products.create({
+          name: products[i].name,
+        });
+
+        const price = await stripe.prices.create({
+          product: product.id,
+          unit_amount: products[i].price * 100,
+          currency: "aud",
+        });
+
+        line_items.push({
+          price: price.id,
+          quantity: 1,
+        });
+      }
+
+      const session = await stripe.checkout.sessions.create({
+        payment_method_types: ["card"],
+        line_items,
+        mode: "payment",
+        success_url: `${url}/success?session_id={CHECKOUT_SESSION_ID}`,
+        cancel_url: `${url}/`,
+      });
+
+      return { session: session.id };
+    },
   },
 
   Mutation: {
